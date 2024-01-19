f33e67029ddd7fdc8f7aac2c8cc765d5f9c5467b
TranDuyLam <144343107+TranDuyLam@users.noreply.github.com>
2024-01-19 20:03:16 +0700

    19/1

--- App.js
+++ App.js
@@ -1,35 +1,81 @@
-import { Navigation } from 'react-native-navigation';
-import { Provider } from 'react-redux';
+import React from 'react';
+import { StyleSheet, View, ImageBackground, KeyboardAvoidingView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import MainScreen from './src/screens/Main/MainScreen';
-import WishListScreen from './src/screens/WishList/WishListScreen';
-import ShoppingCartScreen from './src/screens/ShoppingCart/ShoppingCartScreen';
-import SideDrawer from './src/screens/SideDrawer/SideDrawer';
+import Login from './components/login/Login';
+import Home from './components/Home';
+import Register from './components/login/Register';
+import Profile from './components/profile/Profile';
+import Cart from './components/cart/Cart';
+import Product from './components/products/Product';
+import ProductDetail from './components/products/ProductDetail';
 
-import configureStore from './src/store/configureStore';
-import startTabs from './src/screens/MainTabs/startMainTabs';
+const Stack = createNativeStackNavigator();
 
-const store = configureStore();
+export default function App() {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Login">
+   
+      <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+         name="Home"
+         component={Home}
+         options={{ headerShown: false }}
+        />
+        <Stack.Screen
+         name="Register"
+         component={Register}
+         options={{ headerShown: false }}
+        />
+        <Stack.Screen
+         name="Profile"
+         component={Profile}
+         options={{ headerShown: false }}
+        />
+        {/* <Stack.Screen
+         name="Favorite"
+         component={Favorite}
+         options={{ headerShown: false }}
+        /> */}
+         <Stack.Screen
+         name="Cart"
+         component={Cart}
+         options={{ headerShown: false }}
+        />
+        <Stack.Screen
+         name="Product"
+         component={Product}
+         options={{ headerShown: false }}
+        />
+        <Stack.Screen
+         name="ProductDetail"
+         component={ProductDetail}
+         options={{ headerShown: false }}
+        />
 
-Navigation.registerComponent("EcommerceApp.MainScreen", 
-  () => MainScreen,
-  store,
-  Provider
-);
-Navigation.registerComponent("EcommerceApp.WishListScreen", 
-  () => WishListScreen,
-  store,
-  Provider
-);
-Navigation.registerComponent("EcommerceApp.ShoppingCartScreen", 
-  () => ShoppingCartScreen,
-  store,
-  Provider
-);
-Navigation.registerComponent("EcommerceApp.SideDrawer", 
-  () => SideDrawer,
-  store,
-  Provider
-);
 
-export default App = () => startTabs();
+    </Stack.Navigator>
+    
+  
+  </NavigationContainer>    
+  
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+
+
+});
